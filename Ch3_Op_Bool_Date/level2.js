5ad73f5f540ce2df3a2514575f2d47182b9fba25
mankal-27 <mnkalkutagi@gmail.com>
2023-06-24 20:32:18 +0530

    updated level2 for perimeter

--- Ch3_Op_Bool_Date/level2.js
+++ Ch3_Op_Bool_Date/level2.js
@@ -8,8 +8,18 @@ var areaOfTrianngle = function(base, height){
     return base * height
 }
 
+var perimeterOfTrangle = function(a,b,c){
+    return a + b + c
+}
 let base = prompt('Enter Base = ')
 let height = prompt('Enter Height = ')
+let SideA = prompt("Enter Side A = ")
+let SideB = prompt("Enter Side B = ")
+let SideC = prompt("Enter Side C = ")
 console.log(`Base = ${base} , Height = ${height}`)
+console.log(`Side A = ${SideA}, Side B = ${SideB} , Side C = ${SideC}`)
+console.log(`Perimeter Of Triangle For Side A  ${SideA}, Side B  ${SideB} , Side C  ${SideC} = ${perimeterOfTrangle(SideA,SideB,SideC)}`)
 console.log(`Area of Triangle For Base ${base} and Height ${height} ==>> ${areaOfTrianngle(base,height)}`)
+
+
 process.exit(0)
