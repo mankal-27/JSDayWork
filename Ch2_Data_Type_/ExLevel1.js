2d4f145f3b2df2732567ab58bf92dde6d70f2f2c
mankal-27 <mnkalkutagi@gmail.com>
2023-06-24 19:03:25 +0530

    Updated Chapter2 Level1 Exercise

---
+++ Ch2_Data_Type_/ExLevel1.js
@@ -0,0 +1,13 @@
+//Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+var challenge = '30 Days Of JavaScript'
+console.log("Challenge Variable =>>>", challenge)
+console.log("Challenge Variable length =>>", challenge.length)
+console.log("Challenge Variable UpperCase ==>>", challenge.toUpperCase())
+console.log("Challenge Variable UpperCase ==>>", challenge.toLowerCase())
+console.log("Cut (slice) out the first word of the string ==>", challenge.substring(0, challenge.length))
+console.log("Cut (slice) out the first word of the string ==>", challenge.substring(0, 1))
+console.log("Cut (slice) out the first word of the string ==>", challenge.includes('Script'))
+console.log("Cut (slice) out the first word of the string ==>", challenge.split())
+
+
+
