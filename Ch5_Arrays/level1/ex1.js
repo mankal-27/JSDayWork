e84d802d7fd8b058e2d6be272b07c248bf4cec03
mankal-27 <mnkalkutagi@gmail.com>
2023-06-25 12:52:10 +0530

    updated

--- Ch5_Arrays/level1/ex1.js
+++ Ch5_Arrays/level1/ex1.js
@@ -23,3 +23,10 @@ const countries = [
     'Node',
     'MongoDB'
   ]
+
+const arr = []
+const arr5 = [1,2,3,4,5,6]
+console.log("----Length of arr5 = ", arr5.length)
+console.log("First Item = ", arr5[0])
+console.log("Middle Item = ", arr5[3])
+console.log("last Item = ", arr5[arr5.length - 1])
